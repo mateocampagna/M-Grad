9521996dc4dc0f2537d8cae5d3bc4f02710d2265
mateocampagna <matecamp4@gmail.com>
2026-08-26 22:59:16 -0300

    add layer class (h)

---
+++ include/nn/layer.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "module.hpp"
+#include "neuron.hpp"
+#include <vector>
+#include <memory>
+
+class Layer : public Module {
+private:
+  std::vector<Neuron> neurons; 
+
+public:
+  Layer(int nin, int nout);
+
+  // Forward Pass
+  std::vector<std::shared_ptr<Node>> operator()(const std::vector<std::shared_ptr<Node>>& x);
+
+  // recolect params (composite)
+  std::vector<std::shared_ptr<Node>> parameters() const override;
+};
